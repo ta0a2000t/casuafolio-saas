1b0fc9884964b17babd8e19ef0a2c2c6adda0e7d
Taha Al Nufaili <55326983+ta0a2000t@users.noreply.github.com>
2024-03-03 05:22:38 -0500

    just organization

--- insanfolio-saas-app/src/components/loggedInMain/editJourney/profileForm/eventsSections/EventsSections.tsx
+++ insanfolio-saas-app/src/components/loggedInMain/editJourney/profileForm/eventsSections/EventsSections.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Card, Collapse, Form, Popconfirm, Space, message, theme } from 'antd';
+import { Button, Card, Collapse, Form, FormListFieldData, Popconfirm, Space, message, theme } from 'antd';
 import { DeleteOutlined, QuestionCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import EventsSection from './eventsSection/EventsSection';
 
@@ -34,6 +34,60 @@ const EventsSections: React.FC = () => {
     )
   }
 
+  function collapsItems (move:(from: number, to: number) => any , remove: (index: number | number[]) => void,fields: FormListFieldData[], field: FormListFieldData, index: number) {
+    return (
+    <Panel header={`Section ${index + 1}`}
+    key='0'
+    style={{
+      marginBottom: 24, // Adjust as needed for spacing between sections
+      backgroundColor: colorBgLayout, // Use theme color
+    }}
+
+  >
+    {/* Render each EventsSection inside the Card */}
+    <EventsSection name={field.name} sectionIndex={index} />
+
+    {/* Place the delete button at the bottom of the Card */}
+
+
+
+
+    <Space aria-orientation='horizontal' title={`Section ${index + 1}`}>
+
+      <Popconfirm
+        title="Delete the section"
+        description="Are you sure to delete this section?"
+        onConfirm={() => remove(field.name)}
+        icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+      >
+        <Button
+          danger
+          icon={<DeleteOutlined />}
+          style={{ marginTop: '10px', width: '100%' }} // Ensures button is full-width
+        >
+          Delete Section
+        </Button>
+      </Popconfirm>
+
+
+      <Button onClick={() => move(index, index - 1)} block disabled={index === 0}>
+        Up
+      </Button>
+      <Button
+
+        onClick={() => move(index, index + 1)}
+        block
+        disabled={index === fields.length - 1}
+      >
+        Down
+      </Button>
+
+    </Space>
+
+    </Panel>
+    )
+  }
+
 
   return (
     <Form.List name="eventsSections">
@@ -41,61 +95,10 @@ const EventsSections: React.FC = () => {
         return (
           <>
             {fields.map((field, index) => (
-              <Collapse defaultActiveKey={['1']} accordion key={field.key}>
-
-                <Panel header={`Section ${index + 1}`}
-                  key='0'
-                  style={{
-                    marginBottom: 24, // Adjust as needed for spacing between sections
-                    backgroundColor: colorBgLayout, // Use theme color
-                  }}
-
-                >
-                  {/* Render each EventsSection inside the Card */}
-                  <EventsSection name={field.name} sectionIndex={index} />
-
-                  {/* Place the delete button at the bottom of the Card */}
-
-
-
-
-                  <Space aria-orientation='horizontal' title={`Section ${index + 1}`}>
-
-                    <Popconfirm
-                      title="Delete the section"
-                      description="Are you sure to delete this section?"
-                      onConfirm={() => remove(field.name)}
-                      icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
-                    >
-                      <Button
-                        danger
-                        icon={<DeleteOutlined />}
-                        style={{ marginTop: '10px', width: '100%' }} // Ensures button is full-width
-                      >
-                        Delete Section
-                      </Button>
-                    </Popconfirm>
-
-
-                    <Button onClick={() => move(index, index - 1)} block disabled={index === 0}>
-                      Up
-                    </Button>
-                    <Button
-
-                      onClick={() => move(index, index + 1)}
-                      block
-                      disabled={index === fields.length - 1}
-                    >
-                      Down
-                    </Button>
-
-                  </Space>
-
-                </Panel>
-
-
-
+              <Collapse defaultActiveKey={['1']} accordion key={field.key} >
+                {collapsItems(move, remove, fields, field, index)}
               </Collapse>
+
 
             ))}
 
